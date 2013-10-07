a225919d329dba715567e7c657638e1d5e915f97
Joel Byrnes <joel@razorit.com>
2013-10-07 17:50:10 -0400

    canvasBuildrr: images work w/ mediaBrowswer, but text broke
    This commit contains a WIP of moving into RequireJS, to hopefully mitigate against scope issues.

---
+++ webroot/js/canvasBuildrr/app.js
@@ -0,0 +1,14 @@
+// Filename: app.js
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone){
+  var initialize = function(){
+     //var mv = new MediaItemsView();
+  };
+
+  return {
+    initialize: initialize
+  };
+});
--- webroot/js/canvasBuildrr/views/TextEdit.js
+++ webroot/js/canvasBuildrr/views/TextEdit.js
@@ -1,27 +1,31 @@
 var TextEditView = Backbone.View.extend({
+	
 	initialize: function( attrs ) {
 		this.options = attrs;
 		this.render();
-		$('select[name="colorpicker"]').simplecolorpicker({picker: true});
+		console.log(Backbone.$('select[name="colorpicker"]'));
+		Backbone.$('select[name="colorpicker"]').simplecolorpicker({picker: true});
 	},
+	
 	render: function() {
 		var template = _.template($("#template-textEdit").html(), this.options);
 		this.$el.append(template);
 
-		this.$el.find('option[value="'+this.model.get('fontColor')+'"]').attr("selected", "selected");
-		this.$el.find('option[value="'+this.model.get('fontSize')+'"]').attr("selected", "selected");
-		this.$el.find('input#font').val(this.model.get('fontFamily'));
+		this.$('option[value="'+this.model.get('fontColor')+'"]').attr("selected", "selected");
+		this.$('option[value="'+this.model.get('fontSize')+'"]').attr("selected", "selected");
+		this.$('input#font').val(this.model.get('fontFamily'));
 
 		if ( this.model.get('isEditable') === false ) {
-			this.$el.find('.cb_lock i').attr('class', 'icon-ban-circle');
+			this.$('.cb_lock i').attr('class', 'icon-ban-circle');
 		} else {
-			this.$el.find('.cb_lock i').attr('class', 'icon-ok-circle');
+			this.$('.cb_lock i').attr('class', 'icon-ok-circle');
 		}
 
 		$("#cb_canvasWrapper").parent().unbind();
 
 		return this;
 	},
+	
 	events: {
 		"keyup .textInput": "updateText",
 		"click .cb_addEditText": "falseHandler",
@@ -34,46 +38,55 @@ var TextEditView = Backbone.View.extend({
 		'change select[name="colorpicker"]': 'updateColor',
 		'change select[name="fontsizepicker"]': 'updateFontsize',
 	},
+	
 	toggleLock: function( event ) {
 		this.model.set('isEditable', !this.model.get('isEditable'));
 		if ( this.model.get('isEditable') === false ) {
-			this.$el.find('.cb_lock i').attr('class', 'icon-ban-circle');
+			this.$('.cb_lock i').attr('class', 'icon-ban-circle');
 		} else {
-			this.$el.find('.cb_lock i').attr('class', 'icon-ok-circle');
+			this.$('.cb_lock i').attr('class', 'icon-ok-circle');
 		}
 	},
+	
 	orderUp: function( event ) {
 		this.model.set('order', this.model.get('order') + 1);
 	},
+	
 	orderDown: function( event ) {
 		this.model.set('order', this.model.get('order') - 1);
 	},
+	
 	toggleFonts: function( event ) {
 		console.log('toggling font list');
 		$("#fontList").fadeToggle();
 	},
+	
 	selectFont: function( event ) {
 		this.model.set('fontFamily', event.target.innerHTML);
 		$("#font")
 			.val(event.target.innerHTML)
 			.css('font-family', event.target.innerHTML);
 		$("#fontList").fadeOut();
-		console.log('fading out font list');
 		return false;
 	},
+	
 	updateText: function( event ) {
 		console.log(this.model.cid);
 		this.model.set('content', event.target.value);
 	},
+	
 	updateColor: function( event ) {
 		this.model.set('fontColor', event.target.value);
 	},
+	
 	updateFontsize: function( event ) {
 		this.model.set('fontSize', event.target.value);
 	},
+	
 	falseHandler: function( event ) {
 		return false;
 	},
+	
 	close: function( event ) {
 		if ( this.model.get('content') === '' ) {
 			this.model.destroy();
@@ -86,7 +99,7 @@ var TextEditView = Backbone.View.extend({
 			}
 		}
 		$('select[name="colorpicker"]').simplecolorpicker('destroy');
-		this.$el.find('.cb_addEditText').remove();
+		this.$('.cb_addEditText').remove();
 	}
 });
 
