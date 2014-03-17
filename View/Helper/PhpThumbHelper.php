72ee60b5d2e1cb22fc87b05feacd7278efd78b2d
Joel Byrnes <joel@razorit.com>
2014-03-17 11:37:54 -0400

    removes whitespace

--- View/Helper/PhpThumbHelper.php
+++ View/Helper/PhpThumbHelper.php
@@ -3,7 +3,7 @@
 App::uses('HtmlHelper', 'View/Helper');
 
 class PhpThumbHelper extends HtmlHelper {
-    
+
     private $php_thumb;
     private $options;
     private $file_extension;
@@ -11,18 +11,18 @@ class PhpThumbHelper extends HtmlHelper {
     private $thumb_data;
     private $error;
     private $error_detail;
-    
+
     private function init($options = array())    {
         $this->options = $options;
         $this->set_file_extension();
         $this->thumb_data = array();
         $this->error = 0;
     }
-    
+
     private function set_file_extension()    {
         $this->file_extension = substr($this->options['src'], strrpos($this->options['src'], '.'), strlen($this->options['src']));
     }
-    
+
 /**
  * This might also be a good spot to do a CDN
  */
@@ -30,33 +30,33 @@ class PhpThumbHelper extends HtmlHelper {
         ksort($this->options);
         $filename_parts = array();
         $cacheable_properties = array('src', 'new', 'w', 'h', 'wp', 'hp', 'wl', 'hl', 'ws', 'hs', 'f', 'q', 'sx', 'sy', 'sw', 'sh', 'zc', 'bc', 'bg', 'fltr', 'far');
-        
+
         foreach($this->options as $key => $value)    {
             if(in_array($key, $cacheable_properties))    {
                 $filename_parts[$key] = $value;
             }
         }
-        
+
         $this->cache_filename = '';
-        
+
         foreach($filename_parts as $key => $value)    {
             $this->cache_filename .= $key . $value;
         }
-        
+
         $last_modified = date("F d Y H:i:s.", filectime($this->options['src']));
         $this->cache_filename = $this->options['save_path'] . 'tmp' . DS . md5($this->cache_filename . $last_modified) . $this->file_extension;
     }
-    
+
     private function image_is_cached() {
         if(is_file($this->cache_filename))    {
             return true;
         }
         return false;
     }
-    
+
     private function create_thumb() {
         App::import('Vendor', 'Media.Vendor', array('file' => 'PhpThumb'.DS.'phpthumb.class.php'));
-    	
+
         $this->php_thumb = new phpThumb();
         $this->php_thumb->setParameter('config_allow_src_above_docroot', true);
         foreach($this->php_thumb as $var => $value) {
@@ -77,29 +77,29 @@ class PhpThumbHelper extends HtmlHelper {
  */
     private function get_thumb_data() {
     	$this->thumb_data['error'] = $this->error;
-    	
+
         if($this->error)    {
             $this->thumb_data['error_detail'] = $this->error_detail;
             $this->thumb_data['src'] = $this->options['error_image_path'];
         } else    {
             $this->thumb_data['src'] = $this->options['display_path'] . '/' . substr($this->cache_filename, strrpos($this->cache_filename, DS) + 1, strlen($this->cache_filename));
         }
-        
+
         if(isset($this->options['w']))    {
             $this->thumb_data['w'] = $this->options['w'];
         } else {
             $this->thumb_data['w'] = null;
         }
-        
+
         if(isset($this->options['h']))    {
              $this->thumb_data['h'] = $this->options['h'];
         } else {
             $this->thumb_data['h'] = null;
         }
-        
+
         return $this->thumb_data;
     }
-    
+
 /**
  * Validate method
  */
@@ -109,9 +109,9 @@ class PhpThumbHelper extends HtmlHelper {
     		$this->error_detail = 'File ' . $this->options['src'] . ' does not exist';
     		return;
     	}
-    	
+
     	$valid_extensions = array('.gif', '.jpg', '.jpeg', '.png');
-    	
+
     	if(!in_array(strtolower($this->file_extension), $valid_extensions))	{
     		$this->error = 1;
     		$this->error_detail = 'File ' . $this->options['src'] . ' is not a supported image type';
@@ -124,9 +124,9 @@ class PhpThumbHelper extends HtmlHelper {
  */
     public function generate($options = array())    {
     	$this->init($options);
-    	
+
     	$this->validate();
-    	
+
     	if(!$this->error)    {
     		$this->set_cache_filename();
         	if(!$this->image_is_cached())    {
